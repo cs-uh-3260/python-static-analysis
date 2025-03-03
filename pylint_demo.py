64ed498b7ec765aa2285397870e4e55463fa7f78
Kristina <ks6304@nyu.edu>
2025-03-03 15:23:32 +0400

    Fix docstring issues

--- pylint_demo.py
+++ pylint_demo.py
@@ -1,10 +1,15 @@
 def my_sum(a, b):
+    '''
+    print sum of two variables'''
     return a + b
 
 
 def hello(name):
+    '''
+    Prints name of person
+    '''
     print(f"Hello, {name}!")
 
 
 hello("Alice")
-hello()
+hello("Bob")
